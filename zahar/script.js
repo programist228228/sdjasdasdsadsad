cecdb26fac949f21b106ae398117cf9aa41506a0
ПРОГРАМИСТ <73749236+programist228228@users.noreply.github.com>
2021-03-27 17:41:35 +0300

    Add files via upload

---
+++ zahar/script.js
@@ -0,0 +1,5 @@
+const leftArrow = document.getElementsByClassName('arrow left')[0];
+const rightArrow = document.getElementsByClassName('arrow right')[0];
+
+const slides = document.getElementsByClassName('slide');
+console.log(slides);
