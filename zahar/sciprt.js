63c5b45d352886aab3fb59b84d1cd26ac42d27db
ПРОГРАМИСТ <73749236+programist228228@users.noreply.github.com>
2021-04-03 17:47:30 +0300

    Add files via upload

---
+++ zahar/sciprt.js
@@ -0,0 +1,30 @@
+const leftArrow = document.getElementsByClassName('arrow left')[0];
+const rightArrow = document.getElementsByClassName('arrow right')[0];
+
+const slide = document.getElementsByClassName('slide');
+const dotsArea = document.querySelector('.dots');
+const dots = document.getElementsByClassName('dot');
+const slides = document.getElementsByClassName('slide');
+
+let = index = 0;
+
+function  showSlide(i){
+    for (let j = 0; j < slides.length; j++) {
+     slides[j].classList.remove('active')
+    }
+    slides[i].classList.add('active')
+    index = i;
+     dots[index].checked = true;
+ }
+
+    
+
+leftArrow.addEventListener('click', () => {
+let currentIndex = index === 0 ? slides.length - 1 : index - 1;
+showSlide(currentIndex);
+})
+
+rightArrow.addEventListener('click', () => {
+    let currentIndex = index === slides.length - 1 ? 0 : index + 1;
+    showSlide(currentIndex);
+    })
